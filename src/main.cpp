853b09539386f87a1daae22be18b3a661d80e188
Xavier Beatrice <Xavbeat03@gmail.com>
2024-04-16 18:41:30 -0400

    feat: Finished some formatting and writing out the extra maze stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,17 @@
 
 int main()
 {
+    //Maze
+
     std::string File = "../../mazes/maze.txt";
 
     Maze::ingestNodesFromFile(File);
 
     std::vector<Node*>* path = Maze::A_Star(Node::getNode(1), Node::getNode(100));
+
+    std::cout << "\nFile:";
+
+    std::cout << "\n" << File << ":\n";
 
     std::cout << "Path:\n";
 
@@ -19,5 +25,58 @@ int main()
         if(i != path->size()-1)
             std::cout << "->";
     }
-    std::cout << "\nPath Length: " << path->at(path->size()-1)->getFCost();
+    std::cout << "\nPath Cost: " << path->at(path->size()-1)->getFCost();
+
+    std::cout << '\n';
+
+    //test-maze
+
+    Node::clearNodes();
+
+    std::string File2 = "../../mazes/test-maze.txt";
+
+    Maze::ingestNodesFromFile(File2);
+
+    std::vector<Node*>* path2 = Maze::A_Star(Node::getNode(1), Node::getNode(6));
+
+    std::cout << "\nFile:";
+
+    std::cout << "\n" <<  File2 << ":\n";
+
+    std::cout << "Path:\n";
+
+    for(int i = 0; i < path2->size(); i++){
+        std::cout << path2->at(i)->getValue();
+        if(i != path2->size()-1)
+            std::cout << "->";
+    }
+    std::cout << "\nPath Cost: " << path2->at(path2->size()-1)->getFCost();
+
+    std::cout << '\n';
+
+    // test-maze2
+
+    Node::clearNodes();
+
+    std::string File3 = "../../mazes/test-maze2.txt";
+
+    Maze::ingestNodesFromFile(File3);
+
+    std::vector<Node*>* path3 = Maze::A_Star(Node::getNode(1), Node::getNode(7));
+
+    std::cout << "\nFile:";
+
+    std::cout << "\n" << File3 << ":\n";
+
+    std::cout << "Path:\n";
+
+    for(int i = 0; i < path3->size(); i++){
+        std::cout << path3->at(i)->getValue();
+        if(i != path3->size()-1)
+            std::cout << "->";
+    }
+    std::cout << "\nPath Cost: " << path3->at(path3->size()-1)->getFCost();
+
+    std::cout << '\n';
+
 }
