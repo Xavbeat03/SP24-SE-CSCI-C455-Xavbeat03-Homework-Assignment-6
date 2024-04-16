25eb2d8ec073e6a85f1072b2a9504777f937f5ca
Xavier Beatrice <Xavbeat03@gmail.com>
2024-04-16 18:19:56 -0400

    feat: implemented the main class method

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,18 @@
 
 int main()
 {
-    std::string testFile = "../../mazes/test-maze2.txt";
+    std::string File = "../../mazes/maze.txt";
 
-    Maze::ingestNodesFromFile(testFile);
+    Maze::ingestNodesFromFile(File);
 
-    std::vector<Node*>* path = Maze::A_Star(Node::getNode(1), Node::getNode(7));
+    std::vector<Node*>* path = Maze::A_Star(Node::getNode(1), Node::getNode(100));
+
+    std::cout << "Path:\n";
+
+    for(int i = 0; i < path->size(); i++){
+        std::cout << path->at(i)->getValue();
+        if(i != path->size()-1)
+            std::cout << "->";
+    }
+    std::cout << "\nPath Length: " << path->at(path->size()-1)->getFCost();
 }
