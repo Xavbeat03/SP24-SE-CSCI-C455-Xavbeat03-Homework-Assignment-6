fdd2065dc4668bef920613f429a947d679ba006c
Xavier Beatrice <Xavbeat03@gmail.com>
2024-04-16 13:12:31 -0400

    test: Another test for ingestion to ensure its not the issue

--- test/Maze/ingestNodesFromFile.cpp
+++ test/Maze/ingestNodesFromFile.cpp
@@ -25,4 +25,39 @@ namespace {
         ASSERT_EQ(Node::getNode(6)->getAdjacentNodes()->size(), 0);
     }
 
+    TEST(IngestNodesFromFile, TEST2){
+        std::string testFile = "../../mazes/test-maze2.txt";
+
+        Maze::ingestNodesFromFile(testFile);
+
+        ASSERT_EQ(Node::NodesToString(), "{1 2 3 4 8 5 6 7 9 10 11 12}");
+
+        ASSERT_EQ(Node::getNode(1)->getAdjacentNodes()->at(0)->getValue(), 2);
+
+        ASSERT_EQ(Node::getNode(2)->getAdjacentNodes()->at(0)->getValue(), 3);
+
+        ASSERT_EQ(Node::getNode(3)->getAdjacentNodes()->at(0)->getValue(), 4);
+        ASSERT_EQ(Node::getNode(3)->getAdjacentNodes()->at(1)->getValue(), 8);
+
+        ASSERT_EQ(Node::getNode(4)->getAdjacentNodes()->at(0)->getValue(), 5);
+
+        ASSERT_EQ(Node::getNode(5)->getAdjacentNodes()->at(0)->getValue(), 6);
+
+        ASSERT_EQ(Node::getNode(6)->getAdjacentNodes()->at(0)->getValue(), 7);
+
+        ASSERT_EQ(Node::getNode(7)->getAdjacentNodes()->size(), 0);
+
+        ASSERT_EQ(Node::getNode(8)->getAdjacentNodes()->at(0)->getValue(), 9);
+
+        ASSERT_EQ(Node::getNode(9)->getAdjacentNodes()->at(0)->getValue(), 10);
+
+        ASSERT_EQ(Node::getNode(10)->getAdjacentNodes()->at(0)->getValue(), 11);
+
+        ASSERT_EQ(Node::getNode(11)->getAdjacentNodes()->at(0)->getValue(), 12);
+
+        ASSERT_EQ(Node::getNode(12)->getAdjacentNodes()->size(), 0);
+
+
+    }
+
 }
